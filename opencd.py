3bef5824fefb5128558edc294b151bfed03393e3
StackStomp <StackStomp@gmail.com>
2016-10-03 22:54:10 +0800

    Reconstruct opencd functions

--- opencd.py
+++ opencd.py
@@ -1,18 +1,4 @@
-
-
-def get_taddress_list(up):
-	l = []
-	for entry in up.get('entries', []):
-		for link in entry.get('links', []):
-			if link.get('type','') == 'application/x-bittorrent':
-				l.append(link.get('href',''))
-	return l
-
-def get_ttitle_list(up):
-	l = []
-	for entry in up.get('entries', []):
-		l.append(entry.get('title', ''))
-	return l
+from default import *
 
 def get_title(up):
 	return "OpenCD"
